e20f788631a0e04139f650875386489e8113c2ef
izikeros <ksafjan@gmail.com>
2017-02-27 05:59:04 +0100

    oh my zsh install added

--- install_root.sh
+++ install_root.sh
@@ -1,5 +1,10 @@
-cat root_packets.txt | xargs apt-get --yes install
+cat packets.txt | xargs apt-get --yes install
+
+# install oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # get visual studio code
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O code.deb
 dpkg -i code.deb
+
+
