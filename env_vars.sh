8e7e8024d273614ae61261d2fd85b381256d38aa
Krystian Safjan <ksafjan@gmail.com>
2017-08-18 12:13:04 +0200

    initial commit for env vars

---
+++ env_vars.sh
@@ -0,0 +1,13 @@
+export ARCHFLAGS="-arch x86_64"
+export WORKON_HOME=$HOME/.virtualenvs
+#export PROJECT_HOME=$HOME/Devel
+export EDITOR='vim'
+#export PATH="$HOME/.tmuxifier/bin:$PATH"
+
+# FIXME: make it working in Fish/Zsh/Bash
+export PATH="$HOME/.screenlayout:$PATH"
+export PATH="$HOME/scripts:$PATH"
+
+export BROWSER=firefox
+export PAGER="most"
+export DJANGO_SETTINGS_MODULE=ebadu.settings
