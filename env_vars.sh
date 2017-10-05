83e7350942995d7d5a0bf86928c85b6d13e3486b
Krystian Safjan <ksafjan@gmail.com>
2017-10-05 08:57:48 +0200

    clean-up

--- env_vars.sh
+++ env_vars.sh
@@ -1,14 +1,11 @@
 export ARCHFLAGS="-arch x86_64"
-export WORKON_HOME=$HOME/.virtualenvs
-#export PROJECT_HOME=$HOME/Devel
+#export WORKON_HOME=$HOME/.virtualenvs
 export EDITOR='vim'
-#export PATH="$HOME/.tmuxifier/bin:$PATH"
 
 export PATH="$HOME/.screenlayout:$PATH"
 export PATH="$HOME/.scripts:$PATH"
 export PATH="$HOME/external/restic:$PATH"
-export PATH="$HOME/external/firefox:$PATH"
-
+export PATH="$HOME/external/pip-pop/bin:$PATH"
 export BROWSER=chromium-browser
 export PAGER="most"
-export DJANGO_SETTINGS_MODULE=ebadu.settings
+#export DJANGO_SETTINGS_MODULE=ebadu.settings
