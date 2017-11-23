1c3e69acffbd70af18db26759a4166adb87b82cf
Krystian Safjan <krystian.safjan@wilabs.com>
2017-11-23 11:48:53 +0100

    basic packages updated

--- bootstrap/install_basic_packages.sh
+++ bootstrap/install_basic_packages.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-log_msg "Updating repo list"
 apt-get update
 
-#TODO: add ppa
-
 # install minimal set of packages from curated list
-log_msg "Initiating install of basics"
-apt-get install -y git mc ranger rofi htop curl wget ran rscync zsh openssh-client openssh-server python-pip
+apt-get install -y vim git mc ranger rofi htop curl wget rsync zsh openssh-client openssh-server python-pip
