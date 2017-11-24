33c8429567405b2bc8fe6f5924a6f343712374dd
Krystian Safjan <ksafjan@gmail.com>
2017-11-24 20:55:11 +0100

    file restored

---
+++ bootstrap/install_basic_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run this as su
+
+echo "Updating repo list"
+apt-get update
+
+#TODO: add ppa
+
+# install minimal set of packages from curated list
+echo "Initiating install of basics"
+apt-get install -y git mc htop curl wget rscync zsh openssh-client openssh-server python-pip terminator
+apt-get install -y ranger rofi tree
