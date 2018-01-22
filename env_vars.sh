225868543b9cf24adb4ea2497dd354768807ce0c
Krystian Safjan <krystian.safjan@wilabs.com>
2018-01-22 10:38:22 +0100

    restic path added

--- env_vars.sh
+++ env_vars.sh
@@ -6,6 +6,7 @@ export PATH="$HOME/.screenlayout:$PATH"
 export PATH="$HOME/.scripts:$PATH"
 export PATH="$HOME/external/pip-pop/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
+export PATH="$HOME/external/restic:$PATH"
 export BROWSER=chromium-browser
 export PAGER="most"
 #export DJANGO_SETTINGS_MODULE=ebadu.settings
