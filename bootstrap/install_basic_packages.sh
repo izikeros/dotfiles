8b53adef3cc8b51bb4f817397cd3fab138b8478b
Krystian Safjan <krystian.safjan@wilabs.com>
2018-01-25 11:17:58 +0100

    support for ubuntu and Arch added

--- bootstrap/install_basic_packages.sh
+++ bootstrap/install_basic_packages.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 # run this as su
 
-echo "Updating repo list"
-apt-get update
+# TODO: read list from file
 
-#TODO: add ppa
+# TODO: recognize system (Arch/Ubuntu)
+SYS=""
+uname -a | grep -q  "Ubuntu" && SYS="ubuntu"
+uname -a | grep -q  "Arch" && SYS="arch"
+if [ "$SYS" == "ubuntu" ]; then
+    #TODO: add ppa repositories(?, perhaps not in this script)
+    echo "-- Ubuntu Linux detected, using apt-get"
+    echo "-- Updating repo list"
+    apt-get update
+    CMD="apt-get install -y"
+fi
+
+if [ "$SYS" == "arch" ]; then
+    CMD="apt-get install -y"
+    echo "Arch Linux detected, using pacman"
+    CMD="pacman -Sy"
+fi
+
 
 # install minimal set of packages from curated list
 echo "Initiating install of basics"
-apt-get install -y git mc htop curl wget rsync zsh openssh-client openssh-server python-pip terminator
-apt-get install -y ranger rofi tree
+$CMD git mc htop curl wget rsync zsh openssh-client openssh-server python-pip terminator
