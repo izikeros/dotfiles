efa4d6bbd4eae4b6997c710cf0656868f85a0b0c
Krystian Safjan <krystian.safjan@wilabs.com>
2018-01-29 21:23:49 +0000

    multiple fixes

--- bootstrap/restore_setup.sh
+++ bootstrap/restore_setup.sh
@@ -4,7 +4,7 @@ BACKUP_DIR="$HOME/backup"
 
 exec >> err_file
 
-$CMD=`./get_distro_pkg_install_command.sh`
+CMD=`./get_distro_pkg_install_command.sh`
 
 source ./helper_functions.sh
 
@@ -22,15 +22,15 @@ symlink_dotfile ./dotfiles/.gitconfig ~/.gitconfig
 
 # ======= prepare vim
 # install vim vundle
-if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	mkdir -p ~/.vim/bundle/
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 # install wombat colors
-if [ ! -f ~/.vim/colors/wombat256mod.vim ]; then
+if [ ! -L ~/.vim/colors/wombat256mod.vim ]; then
 	mkdir -p ~/.vim/colors/
-	symlink_dotfile ./dotfiles/wombat256mod.vim ~/.vim/colors/wombat256mod.vim
+	symlink_dotfile ~/dotfiles/wombat256mod.vim ~/.vim/colors/wombat256mod.vim
 fi
 
 # ======== install omzsh
@@ -38,15 +38,25 @@ if [ ! -d ~/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 echo "omzsh should be installed, you might want to switch shell to zsh via command: 'chsh -s /bin/zsh'"
-# TODO: install spaceship theme
+
+# ======== install ZSH spaceship theme
+# TODO: $ZSH_CUSTOM seems to be not set, need to install manually for now
+#git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+#ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # ======== install z
-if [ ! -d ~/.z ]; then
-    git clone https://github.com/rupa/z
+if [ ! -d ~/z ]; then
+    git clone https://github.com/rupa/z ~/z
 fi
 echo "z installed"
 
 # ======== install fzf
+if [ ! -d ~/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
+echo "fzf installed"
+
 # TODO:
 # install ppa repositories
 # install tools from pip packages
