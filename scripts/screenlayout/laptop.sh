0b4dfc60a391e47eb34681a612a5daa82cbd1394
Krystian Safjan <krystian.safjan@wilabs.com>
2018-02-02 23:04:33 +0100

    screen layout screens added to repo

---
+++ scripts/screenlayout/laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1-1 --mode 1600x900 --pos 0x0 --rotate normal --output LVDS-0 --off --output VGA-1-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
