8ad7ca5505366c2641c81f5de2dd68e6985ec997
Krystian Safjan <krystian.safjan@wilabs.com>
2018-02-02 23:03:02 +0100

    system aware install command added, TODO notes added

--- bootstrap/restore/install_from_the_list.sh
+++ bootstrap/restore/install_from_the_list.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # usage: ./install_from_the_list.sh packages_list.txt
+
+# TODO: ignore comments
+#	1. lines starting with #
+#   2. everything after #
+
+# TODO: support for multiple systems
+
+# handle yaourt markings
+
+CMD=`get_distro_pkg_install_command`
 while read package;
 do
 	echo "--- $package ---"
-	apt-get -y install $package
+	$CMD install $package
 	echo ""
 done < $1
