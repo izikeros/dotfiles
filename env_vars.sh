9789afa13165b2dbeb3c1c75a17f1788ea3d5a95
Krystian Safjan <krystian.safjan@wilabs.com>
2018-02-05 14:57:55 +0100

    update for new system and organization

--- env_vars.sh
+++ env_vars.sh
@@ -1,11 +1,10 @@
 export ARCHFLAGS="-arch x86_64"
 export EDITOR='vim'
-export PATH="$HOME/bulk/.screenlayout:$PATH"
+export PATH="$HOME/dotfiles/scripts/screenlayout:$PATH"
 export PATH="$HOME/dotfiles/scripts:$PATH"
+
 export PATH="$HOME/bulk/external/pip-pop/bin:$PATH"
-export PATH="$HOME/go/bin:$PATH"
-export PATH="$HOME/bulk/external/restic:$PATH"
-export PATH="$HOME/bulk/external/Ryver:$PATH"
-export BROWSER=chromium-browser
+
+export BROWSER="chromium"
 export PAGER="most"
 export DJANGO_SETTINGS_MODULE=ebadu.settings
