8e3897f6096f5a7538d4d66e4969131f350be94e
Krystian Safjan <krystian.safjan@wilabs.com>
2018-02-19 22:19:01 +0100

    absolute path to package list added

--- bootstrap/restore/install_basic_packages.sh
+++ bootstrap/restore/install_basic_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # run this as su
 
-# TODO: read list from file
-
+PKG_PATH=$HOME/dotfiles/bootstrap/pkg
 CMD=`get_distro_pkg_install_command.sh`
 
 # install minimal set of packages from curated list
 echo "Initiating install of basics"
-xargs sudo $CMD < pkg-lst-basic.txt
+
+xargs sudo $CMD < $PKG_PATH/pkg-lst-basic.txt
