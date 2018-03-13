b5f87810d45af09c0b2998f081454ac9bd29ee6f
Krystian Safjan <krystian.safjan@wilabs.com>
2018-03-13 11:25:07 +0100

    script for checking if important repos are clean added

--- scripts/check_if_repos_are_clean.sh
+++ scripts/check_if_repos_are_clean.sh
@@ -3,5 +3,12 @@ list=(~/dotfiles /home/bulk/Dockerfiles)
 
 for repo in "${list[@]}"
 do
-    echo "-- check if git repo: $repo is clean"
+    cwd=`pwd`
+    cd $repo
+    git status | grep -q "nothing to commit, working tree clean" && st="clean" || st="dirty"
+
+    if [ $st == 'dirty' ];then
+        echo "-- git repo: $repo is dirty"
+    fi
 done
+cd $cwd
