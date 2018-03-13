6e02d9dcb206cb9f6c9a175ff3086d4b12707906
Krystian Safjan <krystian.safjan@wilabs.com>
2018-03-13 10:52:14 +0100

    rename env_vars.sh

--- env_and_path.sh
+++ env_and_path.sh
@@ -1,10 +1,11 @@
 export ARCHFLAGS="-arch x86_64"
 export EDITOR='vim'
+export VISUAL='vim'
 export PATH="$HOME/dotfiles/scripts/screenlayout:$PATH"
 export PATH="$HOME/dotfiles/scripts:$PATH"
 
 export PATH="$HOME/bulk/external/pip-pop/bin:$PATH"
 
-export BROWSER="chromium"
+export BROWSER="firefox"
 export PAGER="most"
 export DJANGO_SETTINGS_MODULE=ebadu.settings
