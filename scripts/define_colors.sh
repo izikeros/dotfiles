7fbe62aab44a7684dfaa54f38f5109b9bde99079
Krystian Safjan <krystian.safjan@wilabs.com>
2018-03-13 14:48:57 +0100

    template script with colors added

---
+++ scripts/define_colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# scriptname - description of script
+
+# Text color variables
+txtund=$(tput sgr 0 1)          # Underline
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}*${txtrst}
+ques=${bldblu}?${txtrst}
+
+echo "wer $txtbld ertqwer $txtrst"
+
