aad481a03e9676837df7b43d32937cdabfe0f280
Krystian Safjan <krystian.safjan@wilabs.com>
2018-04-05 21:57:41 +0200

    counting files in dirs

---
+++ scripts/count_files.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+#
+# count files in each dir in current directory
+#
+
+find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' i ; do
+    echo -n $i": " ;
+    (find $i -type f | wc -l) ;
+done
