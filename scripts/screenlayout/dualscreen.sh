a789994a7c03fed0607ca15dad412b8ba665ed31
Krystian Safjan <krystian.safjan@wilabs.com>
2018-06-08 09:01:32 +0200

    dualscreen updated

--- scripts/screenlayout/dualscreen.sh
+++ scripts/screenlayout/dualscreen.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xrandr --output LVDS-1-1 --mode 1600x900 --pos 0x176 --rotate normal --output LVDS-0 --off --output VGA-1-1 --off --output HDMI-0 --off --output DP-3 --off --output DP-2 --off --output DP-1 --primary --mode 1920x1200 --pos 1600x0 --rotate normal --output DP-0 --off
+xrandr \
+    --output LVDS-1 --mode 1600x900 --pos 0x176 --rotate normal \
+    --output LVDS-0 --off \
+    --output VGA-1-1 --off \
+    --output HDMI-0 --off \
+    --output DP-3 --off \
+    --output DP-2 --off \
+    --output DP-1-2 --primary --mode 1920x1200 --pos 1600x0 --rotate normal \
+    --output DP-0 --off
+
