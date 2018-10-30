81b805bb42dc81c8ed716a16a1998657e1445c65
Krystian Safjan <ksafjan@gmail.com>
2018-10-30 10:12:11 +0100

    Add conditional checking if repo is clean
    check only if repo exist

--- scripts/check_if_repos_are_clean.sh
+++ scripts/check_if_repos_are_clean.sh
@@ -16,21 +16,25 @@ list=(~/dotfiles /home/bulk/dockerfiles)
 for repo in "${list[@]}"
 do
     cwd=`pwd`
-    cd $repo
-    git status | grep -q "nothing to commit, working tree clean" && st="clean" || st="dirty"
+    if [ -d $repo ]; then
+	cd $repo
+	git status | grep -q "nothing to commit, working tree clean" && st="clean" || st="dirty"
 
-    num_commits=`git cherry -v | wc -l`
+	num_commits=`git cherry -v | wc -l`
 
-    push_txt=""
-    if [ $num_commits -gt 0 ];then
-        push_txt=" # commits to push: $txtbld$num_commits.$txtrst"
+	push_txt=""
+	if [ $num_commits -gt 0 ];then
+    	    push_txt=" # commits to push: $txtbld$num_commits.$txtrst"
+	fi
+
+	status_txt="-- $bldblu$repo$txtrst is clean"
+	if [ $st == 'dirty' ];then
+    	    status_txt="-- $bldblu$repo$txtrst is $bldred dirty$txtrst"
+	fi
+
+	echo "$status_txt $push_txt"
+    else
+	echo "repo $repo does not exist, skipping"
     fi
-
-    status_txt="-- $bldblu$repo$txtrst is clean"
-    if [ $st == 'dirty' ];then
-        status_txt="-- $bldblu$repo$txtrst is $bldred dirty$txtrst"
-    fi
-
-    echo "$status_txt $push_txt"
 done
 cd $cwd
