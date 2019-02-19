1a7530a0e98c9bbab57a0d089e67ec4af9d27784
Krystian Safjan <ksafjan@gmail.com>
2019-02-19 20:41:49 +0100

    Correct restore scripts

--- restore/15b-symlink-polybar-config-and-scripts.sh
+++ restore/15b-symlink-polybar-config-and-scripts.sh
@@ -6,4 +6,7 @@ PTH=".config/polybar"
 symlink_dotfile "$HOME/dotfiles/$PTH/config" "$HOME/$PTH/config"
 symlink_dotfile "$HOME/dotfiles/$PTH/master.conf" "$HOME/$PTH/master.conf"
 symlink_dotfile "$HOME/dotfiles/$PTH/modules.conf" "$HOME/$PTH/modules.conf"
-symlink_dotfile "/home/bulk/polybar-scripts/polybar-scripts" "$HOME/polybar-scripts"
+
+# ensure that there is dir for trash (polybar script is looking for it)
+mkdir -p ~/.local/share/Trash/files
+
