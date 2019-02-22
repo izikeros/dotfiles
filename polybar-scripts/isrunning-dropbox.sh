55674ed2511e1fafa2083859ff78dce2008a2b42
Krystian Safjan <ksafjan@gmail.com>
2019-02-22 06:53:24 +0100

    Custom polybar scripts

---
+++ polybar-scripts/isrunning-dropbox.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+    --toggle)
+        if [ "$(pgrep dropbox)" ]; then
+            pkill -f dropbox
+        else
+            dropbox &
+        fi
+        ;;
+    *)
+        if [ "$(pgrep dropbox)" ]; then
+            echo "Dropbox"
+        else
+            echo "no-Dropbox"
+        fi
+        ;;
+esac
