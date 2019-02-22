2e9667abe621f63c90c562de35443db24b387b1e
Krystian Safjan <ksafjan@gmail.com>
2019-02-22 06:50:43 +0100

    Refresh env variables

--- env_and_path.sh
+++ env_and_path.sh
@@ -1,13 +1,9 @@
 export ARCHFLAGS="-arch x86_64"
 export EDITOR='vim'
 export VISUAL='vim'
-export PATH="$HOME/dotfiles/scripts/screenlayout:$PATH"
 export PATH="$HOME/dotfiles/scripts:$PATH"
 
-export PATH="$HOME/bulk/external/pip-pop/bin:$PATH"
-export PATH="$HOME/.gem/ruby/2.5.0/bin:$PATH"
-
-export BROWSER="chromium"
+export BROWSER="firefox"
 export PAGER="most"
-#export DJANGO_SETTINGS_MODULE=ebadu.settings
-export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+#export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+
