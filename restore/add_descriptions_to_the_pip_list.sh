bc5b63b13a59d0ecddaa409dee5ec9f5532733fe
Krystian Safjan <ksafjan@gmail.com>
2019-03-02 23:28:44 +0100

    Add script for adding package description to the packages list

---
+++ restore/add_descriptions_to_the_pip_list.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# usage: ./install_from_the_list.sh packages_list.txt [--ask]
+
+PKG_LIST_FILE="$1"
+TMP_FILE_IN=/tmp/install_list_in.txt
+TMP_FILE_OUT=/tmp/install_list_out.txt
+CMD=pip
+
+# remove comments
+sed -e "s/#.*$//gi" -e "/^$/d" "$PKG_LIST_FILE" > "$TMP_FILE_IN"
+
+# count packages
+N=$(wc -l "$TMP_FILE_IN")
+
+echo "Found $N packages on the list:"
+
+while read -r package;
+do
+	descr=$($CMD show "$package" | grep 'Summary' | sed 's/Summary\s*:\s*//')
+	printf "%-20s # %s\n" "$package" "$descr"
+	printf "%-20s # %s\n" "$package" "$descr" >> "$TMP_FILE_OUT"
+done < "$TMP_FILE_IN"
+rm "$TMP_FILE_IN"
+mv "$PKG_LIST_FILE" "$PKG_LIST_FILE.bak"
+mv "$TMP_FILE_OUT" "$PKG_LIST_FILE"
