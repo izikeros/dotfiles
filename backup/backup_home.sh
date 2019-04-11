9ec09f6e51ad8c1cb88552e3b37870ac32531558
Krystian Safjan <ksafjan@gmail.com>
2019-04-11 08:31:58 +0200

    Remove not needed files

--- backup/backup_home.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# create list of installed deb packages, sources listm and keys. Finally backup
-# whole home directory of the user
-dpkg --get-selections > ~/Package.list
-sudo cp /etc/apt/sources.list ~/sources.list
-sudo apt-key exportall > ~/Repo.keys
-rsync --progress /home/`whoami` /path/to/user/profile/backup/here
-
-##  Reinstall now
--- restore/symlink-env-and-path.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ./helper_functions.sh
-
-echo "-------- symlinking environmental variables and PATH setup -----------"
-symlink_dotfile ~/dotfiles/env_and_path.sh ~/env_and_path.sh
