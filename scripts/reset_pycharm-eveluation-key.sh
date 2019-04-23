d2ee5652fa8218e3c24f21467b45cd4888f28ac7
Krystian Safjan <ksafjan@gmail.com>
2019-04-23 17:25:30 +0200

    Update script (but still not working)

--- scripts/reset_pycharm-eveluation-key.sh
+++ scripts/reset_pycharm-eveluation-key.sh
@@ -3,26 +3,39 @@
 # https://www.quora.com/Is-there-a-way-to-extends-trial-period-of-Jetbrains-IDE
 
 # evaluation key file location
-PYCHARM_SETTINGS_DIR="$HOME/.PyCharm2018.1"
-EVAL_FILE="PyCharm181.evaluation.key"
-OPTIONS_FILE="options.xml"
-
+PYCHARM_SETTINGS_DIR="$HOME/.PyCharm2019.1"
+EVAL_FILE="PyCharm191.evaluation.key"
 EVAL_KEY_FILE_FULLPATH="$PYCHARM_SETTINGS_DIR/config/eval/$EVAL_FILE"
-OPTIONS_FILE_FULLPATH="$PYCHARM_SETTINGS_DIR/config/options/$OPTIONS_FILE"
+
 
 # Remove evaluation key file
-if [ ! -f "$EVAL_KEY_FILE" ]; then
-	echo "Evaluation key file not found at indicated location."
+echo "Trying to remove evaluation key file"
+if [ ! -f "$EVAL_KEY_FILE_FULLPATH" ]; then
+	echo "Evaluation key file not found at indicated location ($EVAL_KEY_FILE_FULLPATH)."
 	echo "Hint:"
 	find ~/ | grep '.PyCharm'| grep "evaluation.key"
 	exit 1
 else
-	echo "removing evaluation key"
+	echo "removing evaluation key: $EVAL_KEY_FILE_FULLPATH"
 	rm "$EVAL_KEY_FILE_FULLPATH"
 	echo "done"
-	exit 0
 fi
 
-# Clear traces in options file
-sed -i.bak '/evlsprt/d' "$OPTIONS_FILE_FULLPATH"
+# Clear traces in options
+OPTIONS_FILE="other.xml"
+OPTIONS_FILE_FULLPATH="$PYCHARM_SETTINGS_DIR/config/options/$OPTIONS_FILE"
 
+echo "Trying to remove evaluation info from config"
+if [ ! -f "$OPTIONS_FILE_FULLPATH" ]; then
+	echo "options file not found at indicated location."
+	echo "Hint:"
+	find "$PYCHARM_SETTINGS_DIR/config/" -type f -name "*.xml" | xargs grep evlsprt
+	exit 1
+else
+	echo "-- before:"
+	grep evlsprt $OPTIONS_FILE_FULLPATH
+	echo "-- removing eval info"
+	sed -i.bak '/evlsprt/d' "$OPTIONS_FILE_FULLPATH"
+	echo "-- after:"
+	grep evlsprt $OPTIONS_FILE_FULLPATH
+fi
