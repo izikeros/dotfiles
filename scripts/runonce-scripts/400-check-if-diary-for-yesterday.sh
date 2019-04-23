59bd302a762fd35807c99ff12ace01e93e75a3ed
Krystian Safjan <ksafjan@gmail.com>
2019-04-23 17:17:36 +0200

    Add condition for case when there is diary

--- scripts/runonce-scripts/400-check-if-diary-for-yesterday.sh
+++ scripts/runonce-scripts/400-check-if-diary-for-yesterday.sh
@@ -6,10 +6,7 @@ f="$(d_file_yesterday).md"
 
 pth="$HOME/vimwiki/diary/$f"
 
-if [ -s "$pth" ];then
-    a=0
-    echo "there is diary"
-else
+if [ ! -s "$pth" ];then
 	    status_txt="-- nothing written in diary for yesterday (d-y | d-ys to edit)"
 	    echo "$status_txt"
         echo $pth
