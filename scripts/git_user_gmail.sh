26656b91c2d71b67770534918b330ef0a4ead137
Krystian Safjan <ksafjan@gmail.com>
2019-05-21 13:38:43 +0200

    Add scripts for git user setup

---
+++ scripts/git_user_gmail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config user.email "ksafjan@gmail.com"
+git config user.name "Krystian Safjan"
+
---
+++ scripts/git_user_nokia.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config user.email "krystian.safjan@nokia.com"
+git config user.name "Krystian Safjan"
+
