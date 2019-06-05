5eca498a6e5acff9c51b79eb144a0e7ac7c1524e
Krystian Safjan <ksafjan@gmail.com>
2019-06-05 08:13:11 +0200

    Check if projects/_priv dir exist in first place

--- scripts/runonce-scripts/200-check-my-github-projects-over-https.sh
+++ scripts/runonce-scripts/200-check-my-github-projects-over-https.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # list projects from my github configured to be accessed via https instead of ssh
 
-res=$(ls "$HOME"/projects/*/.git/config | xargs grep 'https:\/\/github\.com\/izikeros')
+if [ -d "$HOME"/projects/_priv/]; then
+    res=$(ls "$HOME"/projects/_priv/*/.git/config | xargs grep 'https:\/\/github\.com\/izikeros')
 
-if [ ! -z "$res" ]; then
-    echo "-- consider configuring ssh access for these projects:"
-    echo "$res"
+    if [ ! -z "$res" ]; then
+        echo "-- consider configuring ssh access for these projects:"
+        echo "$res"
+    fi
 fi
-
--- scripts/runonce-scripts/300-check-my-projects-not-git-repo.sh
+++ scripts/runonce-scripts/300-check-my-projects-not-git-repo.sh
@@ -12,18 +12,20 @@ pass=${bldblu}*${txtrst}
 warn=${bldred}*${txtrst}
 ques=${bldblu}?${txtrst}
 
-for dir in $HOME/projects/*/     # list directories in the form "/tmp/dirname/"
-do
-	proj=${dir%*/}      # remove the trailing "/"
-    proj=${proj##*/}	# everything after the final "/"
+if [ -d "$HOME"/projects/_priv ]; then
+    for dir in $HOME/projects/_priv/*/     # list directories in the form "/tmp/dirname/"
+    do
+        proj=${dir%*/}      # remove the trailing "/"
+        proj=${proj##*/}	# everything after the final "/"
 
 
-	if [[ $proj =~ ^\_ ]]; then
-	 	a=0
-	else
-		res=$dir.git/config
-	    if [ ! -e $res ]; then
-	     	echo "    $bldred$proj$txtrst is not a git repo"
-	    fi
-	fi
-done
+        if [[ $proj =~ ^\_ ]]; then
+            a=0
+        else
+            res=$dir.git/config
+            if [ ! -e $res ]; then
+                echo "    $bldred$proj$txtrst is not a git repo"
+            fi
+        fi
+    done
+fi
