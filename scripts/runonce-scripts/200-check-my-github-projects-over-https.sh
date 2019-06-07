480956f2ca7b917b54062502906dfe07c4029669
Krystian Safjan <ksafjan@gmail.com>
2019-06-07 23:23:20 +0200

    Fix bug

--- scripts/runonce-scripts/200-check-my-github-projects-over-https.sh
+++ scripts/runonce-scripts/200-check-my-github-projects-over-https.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # list projects from my github configured to be accessed via https instead of ssh
 
-if [ -d "$HOME"/projects/_priv/]; then
+if [ -d "$HOME"/projects/_priv/ ]; then
     res=$(ls "$HOME"/projects/_priv/*/.git/config | xargs grep 'https:\/\/github\.com\/izikeros')
 
-    if [ ! -z "$res" ]; then
+    if [ -n "$res" ]; then
         echo "-- consider configuring ssh access for these projects:"
         echo "$res"
     fi
