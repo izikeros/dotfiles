ee8a990e4859ee18572461fcfbb9f5e10c3673d6
Krystian Safjan <ksafjan@gmail.com>
2019-06-13 12:36:41 +0200

    Add dircolors action and add it to shell actions list

--- restore/shell_acc.sh
+++ restore/shell_acc.sh
@@ -2,8 +2,9 @@
 050-symlink-zsh-realated-files-zgen.sh
 051-run-zgen.sh
 053-symlink-powerlevel9k-config.sh
+054-set-powerlevel9k-default-user.sh
+056-symlink-dircolors.sh
 060-symlink-gitconfig.sh
 063-add-global-gitignore.sh
 070-install-fzf.sh
 080-install-vim.sh
-054-set-powerlevel9k-default-user.sh
