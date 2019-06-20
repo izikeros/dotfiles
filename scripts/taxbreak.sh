14bf92eaab99a80b18bec5e596e51b1318d147c5
Krystian Safjan <ksafjan@gmail.com>
2019-06-20 21:40:33 +0200

    Automate date for taxbreak script

--- scripts/taxbreak.sh
+++ scripts/taxbreak.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=short --since='1 month' | grep "Krystian Safjan" | grep "2019-05" > ~/Documents/taxbreak/2019-05-24/2019_05_taxbreak.txt
+DATE=$(date  +%Y-%m)
+git log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=short --since='1 month' | grep "Krystian Safjan" | grep "$DATE" > ~/Documents/taxbreak/${DATE}_taxbreak.txt
