af818e27f8fa5536f24942fcc2812850f6b56b23
Krystian Safjan <ksafjan@gmail.com>
2019-07-06 05:56:44 +0200

    Change default browser to Brave

--- env_and_path.sh
+++ env_and_path.sh
@@ -3,7 +3,7 @@ export EDITOR='vim'
 export VISUAL='vim'
 export PATH="$HOME/dotfiles/scripts:$PATH"
 
-export BROWSER="firefox"
+export BROWSER="brave"
 if hash most 2>/dev/null; then
 	export PAGER="most"
 else
