70af2bc7bfa82f3560113f51ba5576a8665a2ae9
Krystian Safjan <ksafjan@gmail.com>
2019-07-06 05:57:36 +0200

    Update

--- restore/update_ext_list.sh
+++ restore/update_ext_list.sh
@@ -10,26 +10,30 @@ INPUT_FILE_2=./pkg/ext-rare.txt
 TMP_FILE_LIST=/tmp/pkg_list_restore.txt
 TMP_FILE_INSTALLED=/tmp/pkg_list_currently_installed.txt
 
+# Prepare current lists for analysis
 # remove comments from the list and remove trailing white spaces
-sed -e "s/#.*$//gi" -e "/^$/d" "$INPUT_FILE_1" | sed 's/\s*$//g'  | sort -f > $TMP_FILE_LIST
-sed -e "s/#.*$//gi" -e "/^$/d" "$INPUT_FILE_2" | sed 's/\s*$//g'  | sort -f >> $TMP_FILE_LIST
+sed -e "s/#.*$//gi" -e "/^$/d" "$INPUT_FILE_1" | sed 's/\s*$//g'  > $TMP_FILE_LIST
+sed -e "s/#.*$//gi" -e "/^$/d" "$INPUT_FILE_2" | sed 's/\s*$//g'  >> $TMP_FILE_LIST
 
+sort -bufi -o $TMP_FILE_LIST $TMP_FILE_LIST
 
 # create list from currently installed packages (installed explicite, not as dependency)
 # TODO play with option -t, -tt
 pacman -Qqe |  grep -v "$(pacman -Qqm)" | grep -v "$(pacman -Qqg base)" |grep -v "$(pacman -Qqd)" | sed 's/\s*$//g' | sort -f > $TMP_FILE_INSTALLED
 
+./add_package_descriptions.sh $TMP_FILE_LIST
+./add_package_descriptions.sh $TMP_FILE_INSTALLED
+
+echo "On the list:$TMP_FILE_LIST, installed:$TMP_FILE_INSTALLED"
 echo "packages on your system that are not on the restore list:"
-#comm -13 $TMP_FILE_LIST $TMP_FILE_INSTALLED
+comm -13 $TMP_FILE_LIST $TMP_FILE_INSTALLED
 
-#comm -13 $TMP_FILE_LIST $TMP_FILE_INSTALLED | wc -l
-#echo ""
-#echo "Running Meld for list comparison"
-#meld $TMP_FILE_LIST $TMP_FILE_INSTALLED
+comm -13 $TMP_FILE_LIST $TMP_FILE_INSTALLED | wc -l
+echo ""
+echo "Running Meld for list comparison"
+meld $TMP_FILE_LIST $TMP_FILE_INSTALLED
 
-#./add_package_descriptions.sh $TMP_FILE_LIST
+cat $TMP_FILE_LIST
 
-#cat $TMP_FILE_LIST
-
-#echo "If list list looks fine, copy it to dotfiles:"
-#echo "cp $TMP_FILE_LIST $INPUT_FILE"
+echo "If list list looks fine, copy it to dotfiles:"
+echo "cp $TMP_FILE_LIST $INPUT_FILE"
