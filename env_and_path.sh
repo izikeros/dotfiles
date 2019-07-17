4296e45009e660c1cd1b23f098a8b3ce749f3d70
Krystian Safjan <ksafjan@gmail.com>
2019-07-17 09:33:59 +0200

    Set default pager to less

--- env_and_path.sh
+++ env_and_path.sh
@@ -4,10 +4,6 @@ export VISUAL='vim'
 export PATH="$HOME/dotfiles/scripts:$PATH"
 
 export BROWSER="brave"
-if hash most 2>/dev/null; then
-	export PAGER="most"
-else
-	export PAGER="less"
-fi
+export PAGER="less"
 #export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
