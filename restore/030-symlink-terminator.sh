7e53b5bc6fa03497a597ca5fe54511a252830c54
Krystian Safjan <ksafjan@gmail.com>
2019-07-22 10:23:38 +0200

    Fix terminator config symlink

--- restore/030-symlink-terminator.sh
+++ restore/030-symlink-terminator.sh
@@ -4,8 +4,8 @@ set -e
 
 # shellcheck source=./helper_functions.sh
 source ./helper_functions.sh
-PTH="dotfiles/.config/terminator"
+PTH=".config/terminator"
 
-symlink_dotfile "$HOME/dotfiles/$PTH" "$HOME/$PTH"
+symlink_dotfile "$HOME/dotfiles/dotfiles/$PTH" "$HOME/$PTH"
 
 echo "### Terminator config symlinked"
