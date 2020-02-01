74bbaf17273f1a1504091d53b7a9c34fb6865079
Krystian Safjan <ksafjan@gmail.com>
2020-02-01 17:59:35 +0100

    Change to neovim

--- env_and_path.sh
+++ env_and_path.sh
@@ -1,6 +1,6 @@
 export ARCHFLAGS="-arch x86_64"
-export EDITOR='vim'
-export VISUAL='vim'
+export EDITOR='nvim'
+export VISUAL='nvim'
 export PATH="$HOME/dotfiles/scripts:$PATH"
 
 export BROWSER="brave"
