650ca8091a271ffbf96ad5f85a2f5f4c48c46ae0
Krystian Safjan <ksafjan@gmail.com>
2020-06-30 09:50:51 +0200

    Add save boot time to autostart

---
+++ scripts/save-boot-time.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+FILE=$HOME/boots.log
+if [[ -e $FILE ]]; then
+    journalctl --list-boots | sed 's/^\s\+//' | cut -d ' ' -f2- >> FILE
+    echo "Append to $FILE"
+else
+    journalctl --list-boots | sed 's/^\s\+//' | cut -d ' ' -f2- > $FILE
+    echo "Create $FILE"
+fi
+
+# sort -k2 -u -o $FILE $FILE
+
