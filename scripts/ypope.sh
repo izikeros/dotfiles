2ece6755eb8167e7cfe2b7a18e232e34a408df66
Krystian Safjan <ksafjan@gmail.com>
2020-07-24 06:34:30 +0200

    Update script description

--- scripts/ypope.sh
+++ scripts/ypope.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Yay POPular Exact package
-# find pattern in package name, sort result by popularity
+#
+# ypope - Yay POPular Exact package - find pattern in package name, sort result by popularity
 
 yay -Ss "$1" | grep "$1" | grep -e + | sed -e 's/(+//' | awk -F' ' '{print $3"\t"$1}' | sed 's/aur\///' | sort -nr | head -10
