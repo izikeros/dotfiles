b970ff7ebab1b251c91eb299e52e42e74f7d734d
Krystian Safjan <ksafjan@gmail.com>
2020-10-23 08:28:30 +0200

    Update stock runonce scripts

--- scripts/runonce-scripts/500-stock.sh
+++ scripts/runonce-scripts/500-stock.sh
@@ -1,2 +1,5 @@
-~/dotfiles/scripts/ticker.sh GC=F CNYA ^GSPC BTC-USD IQQH.DE
-~/dotfiles/scripts/bankier.sh ETFSP500 EUR ZLOTO WIG20 MWIG40
+~/dotfiles/scripts/ticker.sh CNYA IQQH.DE
+~/dotfiles/scripts/bankier.sh ETFSP500 EUR
+echo '-----'
+~/dotfiles/scripts/bankier.sh ZLOTO WIG20 MWIG40
+~/dotfiles/scripts/ticker.sh GC=F ^GSPC BTC-USD
