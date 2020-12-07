f2a779939ca234d5d2e929ae0c02d0bf618a3164
Krystian Safjan <ksafjan@gmail.com>
2020-12-07 12:22:26 +0100

    Improve stock scripts

--- scripts/runonce-scripts/500-stock.sh
+++ scripts/runonce-scripts/500-stock.sh
@@ -1,5 +1,9 @@
-~/dotfiles/scripts/ticker.sh CNYA IQQH.DE
-~/dotfiles/scripts/bankier.sh ETFSP500 EUR USD
-echo '-----'
-~/dotfiles/scripts/bankier.sh ZLOTO WIG20 MWIG40
-~/dotfiles/scripts/ticker.sh GC=F ^GSPC BTC-USD
+#!/usr/bin/env bash
+
+# ticker.sh accept Yahoo finance tickers
+~/dotfiles/scripts/ticker.sh $(cat ~/.ticker.conf) | sed 's/CNYA/Chin/' | sed 's/IQQH.DE/Energ  /' | sed 's/RBOT.L/Robot /' | sed 's/GC=F/Gold/' | sed 's/\^GSPC/SP500/'
+
+#~/dotfiles/scripts/bankier.sh ETFSP500
+#echo '-----'
+#~/dotfiles/scripts/bankier.sh ZLOTO WIG20 MWIG40
+#~/dotfiles/scripts/ticker.sh GC=F ^GSPC BTC-USD
