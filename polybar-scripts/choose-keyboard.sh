b70ba673d149ba91a82b5c55753fc45b5c6c3fcb
Krystian Safjan <ksafjan@gmail.com>
2021-02-13 07:32:18 +0100

    Minor updates to config

--- polybar-scripts/choose-keyboard.sh
+++ polybar-scripts/choose-keyboard.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cat ~/.config/keyboards_list | rofi -dmenu -p "Choose keyboard:" -a 0 -no-custom | xargs setxkbmap -model microsoft4000 -layout
+cat ~/.config/keyboards_list | rofi -dmenu -i -p "Choose keyboard:" -no-custom -width 30 -lines 2| xargs setxkbmap -model microsoft4000 -layout
+
+#                 ANS="$(rofi -sep "|" -dmenu -i -p 'System' -width 20 \
+#                    -hide-scrollbar -line-padding 4 -padding 20 \
+#                    -lines 4 <<< " Shutdown| Suspend| Lock| Reboot|")"
