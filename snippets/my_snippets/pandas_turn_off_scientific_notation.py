f66c82e72032297d79a20eb6b84eb6041eed8e5b
Krystian Safjan <ksafjan@gmail.com>
2021-02-13 07:36:01 +0100

    Create foldef with snippets

---
+++ snippets/my_snippets/pandas_turn_off_scientific_notation.py
@@ -0,0 +1,1 @@
+pd.set_option('display.float_format', lambda x: '%.1f' % x)
---
+++ snippets/my_snippets/pandas_wide_column_display.py
@@ -0,0 +1,4 @@
+# pandas - wide display of dataframe
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', -1)
