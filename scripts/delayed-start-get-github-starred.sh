1e363f0643de608a4bc07244bb4efaeec2ab4f1a
Krystian Safjan <ksafjan@gmail.com>
2021-10-13 12:37:01 +0200

    Add and update delayed start scripts

---
+++ scripts/delayed-start-get-github-starred.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Start with delay (at openbox start) refreshing my github stars
+
+sleep 75
+~/dotfiles/scripts/get-github-starred.sh
---
+++ scripts/delayed-start-get-hn-favs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Start with delay (at openbox start) refreshing my HackerNews favs
+
+sleep 1
+~/dotfiles/scripts/get-my-all-hn-favs.sh
